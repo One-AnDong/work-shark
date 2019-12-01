ddabdbb60b4816b28c3755856c4e425867362168
yaokaitao <gitme@foxmail.com>
2019-12-01 22:57:01 +0800

    init pro

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="a" label="测试数据"> </el-table-column>
+      <el-table-column prop="b" label="测试数据2"> </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      tableData: [
+        {
+          a: '1',
+          b: '2'
+        }
+      ]
+    }
   }
 }
 </script>
